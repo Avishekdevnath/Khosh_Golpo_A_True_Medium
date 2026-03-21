f5616f750e46e8014d63b997f644acdc82f5c3c2
Avishek Devnath <avishekdevnath@gmail.com>
2026-03-21 11:31:50 +0600

    refactor: migrate NotificationsWorkspace to Tailwind + feature-folder

---
+++ frontend/src/components/notifications/index.ts
@@ -0,0 +1,5 @@
+export { default } from "./NotificationsWorkspace";
+export { NotificationList } from "./NotificationList";
+export { default as AppealModal } from "./AppealModal";
+export { default as RejectionReasonModal } from "./RejectionReasonModal";
+export type { NotificationListProps, NotificationListHandlers } from "./NotificationList";
