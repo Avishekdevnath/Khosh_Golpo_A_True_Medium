35303f9d7ae46ea27942b5b513d63bcc682aae76
Avishek Devnath <avishekdevnath@gmail.com>
2026-03-21 11:30:43 +0600

    refactor: migrate MessagesWorkspace to Tailwind + feature-folder

---
+++ frontend/src/components/messages/index.ts
@@ -0,0 +1,6 @@
+export { default as MessagesWorkspace } from "./MessagesWorkspace";
+export { default as ConversationListPanel } from "./ConversationListPanel";
+export { default as ChatPanel } from "./ChatPanel";
+export { default as MessageComposer } from "./MessageComposer";
+export { default as ConversationItem } from "./ConversationItem";
+export { default as MessageBubble } from "./MessageBubble";
